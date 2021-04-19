f108e003bd67be360d6cca139e7a3c0d059abe62
kartonixon <adamkartanowicz@gmail.com>
2021-04-19 16:46:21 +0200

    add a way to invalidate user's refresh tokens

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,5 +1,14 @@
-import { Query, Resolver } from "type-graphql";
+import {
+	Arg,
+	Ctx,
+	Mutation,
+	Query,
+	Resolver,
+	UseMiddleware,
+} from "type-graphql";
+import { AuthMiddleware } from "../auth/AuthMiddleware";
 import { User } from "../entity/User";
+import { ExpressContext } from "../ExpressContext";
 
 @Resolver()
 export class UserResolver {
@@ -8,8 +17,27 @@ export class UserResolver {
 		return "hi!";
 	}
 
+	@Query(() => String)
+	@UseMiddleware(AuthMiddleware)
+	currentUserId(@Ctx() { payload }: ExpressContext) {
+		return payload!.userId;
+	}
+
 	@Query(() => [User])
 	users() {
 		return User.find();
+	}
+
+	// TODO: Only for testing
+	@Mutation(() => Boolean)
+	async revokeRefreshTokensForUser(@Arg("userId") userId: string) {
+		const user = await User.findOne(userId);
+
+		if (!user) return false;
+
+		user.tokenVersion += 1;
+		user.save();
+
+		return true;
 	}
 }
